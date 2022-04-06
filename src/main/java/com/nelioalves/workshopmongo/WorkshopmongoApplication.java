db75c3bd9971252089f55343a057210a9937ff7b
José Augusto Mello Crispim <joseaugusto.mello01@gmail.com>
2022-04-06 18:09:11 -0300

    conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
@@ -2,10 +2,18 @@ package com.nelioalves.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.nelioalves.workshopmongo.repository.UserRepository;
+
+
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = UserRepository.class)
 public class WorkshopmongoApplication {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
