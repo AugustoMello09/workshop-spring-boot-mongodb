964352a2123d1d83b8be3a2144ac012ec9c4b9cd
José Augusto Mello Crispim <joseaugusto.mello01@gmail.com>
2022-04-11 10:38:06 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -29,7 +29,14 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-
+	
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	
 	public User FromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
