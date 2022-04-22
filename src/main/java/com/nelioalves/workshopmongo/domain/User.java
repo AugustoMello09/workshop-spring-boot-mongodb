108df3dcbd0692525287fdbc0e17024514f1a412
José Augusto Mello Crispim <joseaugusto.mello01@gmail.com>
2022-04-22 09:52:40 -0300

    Referenciando os posts do usuário

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>(); 
+	
 	public User() {
 		
 	}
@@ -25,7 +31,16 @@ public class User implements Serializable {
 		this.name = name;
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
 
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	
 	public String getId() {
 		return id;
 	}
@@ -66,6 +81,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 	
 }
